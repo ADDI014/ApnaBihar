c71d3e6021721f94297a255f151a062af277be2a
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-09-17 18:38:09 +0530

    corrected some error or correct data main touristplacesspot

--- src/Pages/TouristSpotDetailPage.jsx
+++ src/Pages/TouristSpotDetailPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { touristSpots } from '../data/homePageData';
+import { touristSpots } from '../data/HomePageData';
 
 // Reusable Icon components for better structure
 const InfoIcon = () => <svg className="w-6 h-6 mr-3 text-orange-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>;
